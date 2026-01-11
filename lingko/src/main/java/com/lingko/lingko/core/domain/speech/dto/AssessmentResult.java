8ca999be9bdf48ee7a5dd828325ca6b269b6e1c3
byeok99 <tkdqur3203@gmail.com>
2026-01-11 18:12:03 +0900

    feat: 발음 평가 기능 추가

---
+++ lingko/src/main/java/com/lingko/lingko/core/domain/speech/dto/AssessmentResult.java
@@ -0,0 +1,14 @@
+package com.lingko.lingko.core.domain.speech.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AssessmentResult {
+    private Double accuracyScore;
+    private Double fluencyScore;
+    private Double completenessScore;
+    private Double pronunciationScore;
+    private String recognizedText;
+}
